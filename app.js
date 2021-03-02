6f152917586022561d7511eb2309240b1d1b8cfc
Elizar Pongracz <pelipongracz@gmail.com>
2021-03-02 20:22:12 +0100

    fixed cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(cors());
 const port = 3000;
 
 var five = require("johnny-five");
