80be70ec830d4f032c9f26ab26ab9523151a7c82
Elizar Pongracz <pelipongracz@gmail.com>
2021-03-02 19:29:26 +0100

    v1

--- app.js
+++ app.js
@@ -6,35 +6,67 @@ var five = require("johnny-five");
 var board = new five.Board({port: "COM8"});
 
 
-board.on("ready", function() {
+board.on("ready", function () {
     let led = new five.Led(6);
-    // let photoresistor = new five.Sensor({
-    //     pin: "A0",
-    //     freq: 250
-    // });
-    //
-    // board.repl.inject({
-    //     pot: photoresistor
-    // });\
-    //
-    // photoresistor.on("data", function() {
-    //     console.log(this.value);
-    // });
+    const leds = [
+        {
+            name: 'led3',
+            led: new five.Led(3),
+            state: false,
+            brightness: 250
+        },
+        {
+            name: 'led5',
+            led: new five.Led(5),
+            state: false,
+            brightness: 250
+        },
+        {
+            name: 'led6',
+            led: new five.Led(6),
+            state: false,
+            brightness: 250
+        },
+        {
+            name: 'led9',
+            led: new five.Led(9),
+            state: false,
+            brightness: 250
+        },
+        {
+            name: 'led10',
+            led: new five.Led(10),
+            state: false,
+            brightness: 255
+        },
+        {
+            name: 'led11',
+            led: new five.Led(11),
+            state: false,
+            brightness: 250
+        }];
 
-    app.get('/start', (req, res) => {
+    app.get('/leds', (req, res) => {
+        const ledsDTO = leds.map((led) => {
+            let newLed = {...led};
+            delete newLed.led;
+            return newLed;
+        });
+        res.header("Access-Control-Allow-Origin", "*");
+        res.setHeader('Content-Type', 'application/json');
+        console.log("get leds");
+        res.json(ledsDTO);
+    });
+
+
+    app.post('/leds', (req, res) => {
         led.on();
+        res.header("Access-Control-Allow-Origin", "*");
+        console.log("post leds");
         res.send("led on")
     });
 
-    app.get('/brightness', (req, res) => {
-        led.brightness(req.query.brightness);
-        res.send("brightness level = " + req.query.brightness)
-    });
 
-    app.get('/stop', (req, res) => {
-        led.off();
-        res.send("led off")
-    });
 });
 
 
@@ -44,3 +76,4 @@ app.listen(port, () => {
 
 
 module.exports = app;
+
